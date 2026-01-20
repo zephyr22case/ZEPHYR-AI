feb8a08cd1e455a16d9f9b7fddfbd9d8964a2f8d
zephyr22case <Prosperpaul657@gmail.com>
2026-01-20 14:13:13 +0300

    Set up Express server with WhatsApp route

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const whatsappRoute = require("./routes/whatsapp");
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/whatsapp", whatsappRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
